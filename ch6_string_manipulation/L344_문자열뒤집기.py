727fe1fc5a6f000e802f8e292f549e942f61fe53
Jiyoung <janice10508@gmail.com>
2021-06-13 15:11:02 +0900

    Create L344_문자열뒤집기.py

---
+++ ch6_string_manipulation/L344_문자열뒤집기.py
@@ -0,0 +1,20 @@
+'''
+문자열 뒤집는 함수를 작성하시오. 리턴없이 리스트 내부를 조작하라.
+
+[in]
+["h", "e", "l", "l", "o"]
+
+[out]
+["o", "l", "l", "e", "h"]
+'''
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        # return s
+
+# s = ["h", "e", "l", "l", "o"]
+
+# sol = Solution()
+# print(sol.reverseString(s))
